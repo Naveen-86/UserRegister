d6fcb4d0db288bdff090f8b81df7eaf9cd6bb86c
Naveen <naveentruly86@gmail.com>
2021-05-26 07:48:55 +0530

    Added: First Name

--- src/main/java/assignments/UserRegister.java
+++ src/main/java/assignments/UserRegister.java
@@ -1,7 +1,26 @@
 package assignments;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegister {
+
+    public static String userInput()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter First Name : ");
+        String b1 = s.nextLine();
+        return b1;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
+        boolean fname = Pattern.matches("^[A-Z]{1}[a-z]{2}[a-z]*",UserRegister.userInput());
+        System.out.println(fname);
+        if(fname == false) {
+            System.out.println("Invalid Name,Try Again!");
+            boolean nfname = Pattern.matches("^[A-Z]{1}[a-z]{2}[a-z]*",UserRegister.userInput());
+            System.out.println(nfname);
+        }
     }
 }
